e1aa41a71556b2e78c883be1dba3616744fab6f8
Juan Miguel Guerrero <juanmiguel@existo.es>
2022-10-30 12:30:55 +0100

    Mysql client update

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const mysql = require('mysql');
+const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
 	host: process.env.DB_HOST,
 	database: process.env.DB_DATABASE,
 	user: process.env.DB_USER,
 	password: process.env.DB_PASSWORD,
-	port: process.env.DB_PORT
+	port: process.env.DB_PORT,
 });
 
 
